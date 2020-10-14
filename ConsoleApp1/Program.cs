57b541b981d0f4b2ce41c17f8971cff7c7f47bce
francescobilla <francesco.billa2004@gmail.com>
2020-10-14 09:19:02 +0200

    secondo aggiornamento con aggiunta di scelta del divisore e domanda conn scelta che numero trasformare.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace trasformatore_da_decimale_a_binario
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num = 16, resto;
+            int num, resto, divisore;
+            Console.WriteLine("inserisci il numero decimale:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci il valore con cui vuoi dividere:");
+            divisore = Convert.ToInt32(Console.ReadLine());
             string binario = "";
             while (num > 0)
             {
-                resto = num % 2;
-                num  /= 2;
+                resto = num % divisore;
+                num /= divisore;
                 binario = Convert.ToString(resto) + binario;
             }
-            Console.WriteLine($"il numero binario è:{binario}");
+            Console.WriteLine($"il numero inserito corrisponde in binario: {binario}");
             Console.ReadLine();
         }
     }
