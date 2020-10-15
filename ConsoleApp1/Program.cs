eeb7d65366a43680cc2789a496a113098c61decb
francescobilla <francesco.billa2004@gmail.com>
2020-10-15 09:17:54 +0200

    programma aggiornato che permette adesso di poter inserire qualsiasi divisore.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,14 +9,37 @@ namespace trasformatore_da_decimale_a_binario
             int num, resto, divisore;
             Console.WriteLine("inserisci il numero decimale:");
             num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("inserisci il valore con cui vuoi dividere(2 o 8):");
+            Console.WriteLine("inserisci il valore con cui vuoi dividere:");
             divisore = Convert.ToInt32(Console.ReadLine());
             string binario = "";
             while (num > 0)
             {
                 resto = num % divisore;
+                if(resto == 15)
+                {
+                    binario += "f";
+                }else if(resto == 14)
+                {
+                    binario += "e";
+                } else if (resto == 13)
+                {
+                    binario += "d";
+                }else if (resto == 12)
+                {
+                    binario += "c";
+                }else if (resto == 11)
+                {
+                    binario += "b";
+                }else if (resto == 10)
+                {
+                    binario += "a";
+                }
+                else
+                {
+                    binario = Convert.ToString(resto) + binario;
+                }
                 num /= divisore;
-                binario = Convert.ToString(resto) + binario;
+               
             }
             Console.WriteLine($"il numero inserito corrisponde in binario: {binario}");
             Console.ReadLine();
